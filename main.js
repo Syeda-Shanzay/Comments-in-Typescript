c2beefabd5b767e67d1b44234db8e85ea62eebaf
Syeda-Shanzay <syedashanzay04@gmail.com>
2024-03-24 01:26:03 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Single line comment:
+// Priniting my favprite number in a message format
+var favNum = 3;
+console.log(favNum);
+// Multi line comment:
+/* In this example:
+we declare a variable favColor and initially
+assign it the value Red.
+*/
+var favColor = "Red";
+console.log(favColor);
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Single line comment:
+// Priniting my favprite number in a message format
+let favNum = 3
+console.log(favNum)
+
+// Multi line comment:
+/* In this example:
+we declare a variable favColor and initially
+assign it the value Red.
+*/
+let favColor = "Red"
+console.log(favColor)
